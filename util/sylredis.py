d7d72caa3a169d58b9b6c6ce0184441f252405a3
lewis <venlv2046@gmail.com>
2016-06-15 00:18:20 +0800

    update

--- util/sylredis.py
+++ util/sylredis.py
@@ -6,7 +6,7 @@ Created on May 11, 2016
 import redis
 
 
-pool = redis.ConnectionPool(host='192.168.0.108', port=6379, max_connections=10)
+pool = redis.ConnectionPool(host='127.0.0.1', port=6379, max_connections=10)
 def get_redis():
     return redis.Redis(connection_pool=pool)
 def set_vcode(userid, vcode):

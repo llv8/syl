181efaa41ef25b539d642f79ee5fc3972db45af8
lewis <venlv2046@gmail.com>
2016-06-14 21:44:08 +0800

    update

--- cust/send_mail.py
+++ cust/send_mail.py
@@ -4,25 +4,23 @@ from email.header import Header
 from email.mime.text import MIMEText
 import logging
 import smtplib
+import syl
 
 
 def send_vcode(to_addr, username, vcode):
     logger = logging.getLogger(__name__)
-    from_addr = ""
-    password = ""
-    smtp_server = ""
     content = '''
     您好{username}:您的验证码是{vcode},有效时长为60分钟。在浏览器中使用命令"vcode"验证。
     '''.format(username=username, vcode=vcode)
     msg = MIMEText(content, 'plain', 'utf-8')
-    msg['From'] = from_addr
+    msg['From'] = syl.settings.MAIL['from_addr']
     msg['To'] = to_addr
     msg['Subject'] = Header(u'siyuel验证码', 'utf-8').encode()
     
-    server = smtplib.SMTP(smtp_server, 25)
+    server = smtplib.SMTP(syl.settings.MAIL['smtp_server'], 25)
     try:
-        server.login(from_addr, password)
-        server.sendmail(from_addr, [to_addr], msg.as_string())
+        server.login(syl.settings.MAIL['from_addr'], syl.settings.MAIL['password'])
+        server.sendmail(syl.settings.MAIL['from_addr'], [to_addr], msg.as_string())
         logger.info(to_addr + ' send mail success')
         return True
     except Exception as e:
--- cust/send_sms.py
+++ cust/send_sms.py
@@ -2,23 +2,23 @@
 import logging
 
 import top.api
+import syl
 
 
 def send_vcode(to_phone, username, vcode):
     logger = logging.getLogger(__name__)
     url = 'gw.api.taobao.com'
     port = 80
-    appkey = ''
-    secret = ''
     req = top.api.AlibabaAliqinFcSmsNumSendRequest(url, port)
-    req.set_app_info(top.appinfo(appkey, secret))
+    req.set_app_info(top.appinfo(syl.settings.SMS['appkey'], syl.settings.SMS['secret']))
      
-    # req.extend = "123456"
+    req.extend = "123456"
     req.sms_type = "normal"
-    req.sms_free_sign_name = ""
     req.sms_param = "{\"vcode\":\"" + str(vcode) + "\",\"username\":\"" + username.replace(',', ' ').encode('utf-8') + "\"}"
     req.rec_num = to_phone.encode('utf-8')
-    req.sms_template_code = ""
+    req.sms_template_code = syl.settings.SMS['sms_template_code']
+    req.sms_free_sign_name = "四月儿"
+     
     try:
         resp = req.getResponse()
         logger.info(resp)

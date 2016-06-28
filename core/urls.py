46d7271154501a7bf953ab2b9797f80a1a1425dd
lewis <venlv2046@gmail.com>
2016-06-28 15:49:23 +0800

    update

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,5 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from . import views
 urlpatterns = [
-    url(r'^s/', views.search),
-    url(r'^c/', views.command),
 ]
